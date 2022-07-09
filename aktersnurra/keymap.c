c91294775f4e22d885893a09316ff7f39d20f3d1
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-09 02:14:19 +0200

    Add tap dance to layout

--- aktersnurra/keymap.c
+++ aktersnurra/keymap.c
@@ -2,23 +2,12 @@
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 
 #include QMK_KEYBOARD_H
+#include "tap_dance.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-
-enum ferris_layers {
-  _COLEMAK_DH,
-  _LOWER,
-  _RAISE
-};
-
-enum ferris_tap_dances {
-  Q_ESC,
-  M_ESC,
-  G_ESC
-};
 
 #define KC_HMA GUI_T(KC_A)
 #define KC_HMR ALT_T(KC_R)
@@ -30,20 +19,12 @@ enum ferris_tap_dances {
 #define KC_HME CTL_T(KC_E)
 #define KC_HMN SFT_T(KC_N)
 
-#define LOWER LT(_LOWER, KC_SPC)
-#define RAISE LT(_RAISE, KC_BSPC)
-
-// Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [M_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_M, KC_ESC)
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK_DH] = LAYOUT( /* COLEMAK MOD DH */
     KC_Q,      KC_W,   KC_F,   KC_P,   KC_B,            KC_J,      KC_L,   KC_U,    KC_Y,   KC_SCLN,
     KC_HMA,    KC_HMR, KC_HMS, KC_HMT, KC_G,            TD(M_ESC), KC_HMN, KC_HME,  KC_HMI, KC_HMO,
     KC_Z,      KC_X,   KC_C,   KC_D,   KC_V,            KC_K,      KC_H,   KC_COMM, KC_DOT, KC_SLSH,
-                               KC_ENT,  LOWER,          RAISE,   KC_TAB
+                               KC_ENT,  TD(TD_LOWER),   TD(TD_RAISE),   KC_TAB
   ),
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
