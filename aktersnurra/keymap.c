50a41d1d0d42f7e87ad2d53860ec48e64d318cac
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-08-27 01:56:54 +0200

    Update keymaps

--- aktersnurra/keymap.c
+++ aktersnurra/keymap.c
@@ -31,13 +31,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_EXLM, KC_PERC, KC_DLR,  KC_AT,          KC_PIPE, KC_HASH, KC_ASTR, KC_AMPR, KC_CIRC,
     KC_TRNS, KC_LBRC, KC_LPRN, KC_TRNS, KC_QUOT,        KC_DQT,  KC_TRNS, KC_EQL,  KC_MINS, KC_BSLS,
     KC_TRNS, KC_RBRC, KC_RPRN, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                               KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS
+                               KC_TRNS, KC_TRNS,        KC_BSPC, KC_TRNS
   ),
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
     KC_TRNS, KC_MUTE, KC_VOLU, KC_VOLD, KC_MPLY,        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS,
     KC_LCTL, KC_LALT, KC_TRNS, KC_TRNS, KC_MPRV,        KC_MNXT, KC_TRNS, KC_F1,   KC_F2,    KC_F3,
-                               KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS
+                               KC_TRNS, KC_SPC,         KC_TRNS, KC_TRNS
   )
 };
