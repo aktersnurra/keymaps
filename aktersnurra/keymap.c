fe342ba26937c48f7b8041f2bccff9e3285f309e
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2023-09-14 23:00:21 +0200

    Update layout

--- aktersnurra/keymap.c
+++ aktersnurra/keymap.c
@@ -28,7 +28,7 @@ enum { _COLEMAK_DH, _NAV, _NUM, _SYM, _FUN, _MEDIA } layers;
 enum { M_ESC } tap_dances;
 
 // Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     [M_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_M, KC_ESC)
 };
 
@@ -49,15 +49,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYM] = LAYOUT( /* [> Symbol layer <] */
     KC_GRV,  KC_EXLM, KC_PERC, KC_DLR,  KC_AT,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_HASH, KC_QUOT, KC_ASTR, KC_MINS, KC_EQL,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_SLSH, KC_BSLS, KC_AMPR, KC_CIRC, KC_PIPE,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_HASH, KC_QUOT, KC_ASTR, KC_MINS, KC_EQL,         KC_TRNS, KC_PIPE, KC_AMPR, KC_CIRC, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
                                KC_TRNS, KC_SPC,         KC_TRNS, KC_TRNS
   ),
 
   [_NUM] = LAYOUT( /* [> Number layer <] */
-    KC_TRNS, KC_7,    KC_8,    KC_9,        KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_4,    KC_5,    KC_6,        KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_0,    KC_1,    KC_2,    KC_3,        KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_1,    KC_2,    KC_3,        KC_0,            KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                MO(_MEDIA),  MO(_FUN),        KC_TRNS, KC_TRNS
   ),
 
