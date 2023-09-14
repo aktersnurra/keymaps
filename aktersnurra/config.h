27f775e42b71edb21922353a7e0f8ade650d6ea5
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2023-09-14 23:14:49 +0200

    Update tapping term value to 150

--- aktersnurra/config.h
+++ aktersnurra/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 
