575052dd54d0bfeb95004f5ab4e9a83ee75e4cd6
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2023-10-04 18:02:01 +0200

    Update nav layer by moving brackets/parentheses

--- aktersnurra/keymap.c
+++ aktersnurra/keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NAV] = LAYOUT( /* [> Navigation layer <] */
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_LPRN, KC_RPRN, KC_TRNS, KC_BSLS,
-                               KC_TRNS, KC_TRNS,        KC_BSPC, KC_TRNS
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN,        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
+                               KC_TRNS, KC_TRNS,        KC_BSPC, KC_LSFT
   ),
 
   [_SYM] = LAYOUT( /* [> Symbol layer <] */
