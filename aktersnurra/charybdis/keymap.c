1e94ca773a5a007555f3c846d048bd08bbbf17aa
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2024-01-02 00:16:51 +0100

    Add scroll to rhs home row

--- aktersnurra/charybdis/keymap.c
+++ aktersnurra/charybdis/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
   [_MOU] = LAYOUT( /* [> Mouse layer <] */
     KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_MS_BTN1, KC_MS_BTN2, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_BTN1,    KC_BTN2,    KC_TRNS,        KC_TRNS, KC_WH_D, KC_WH_U, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS
   )
